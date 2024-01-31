ae3271d1ed5fc6b31ac71aada6bbd8e222d83df2
laoning <laoning2008@gmail.com>
2024-02-01 00:27:11 +0800

    coding

--- src/simple_ipc/connector.hpp
+++ src/simple_ipc/connector.hpp
@@ -10,11 +10,129 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string>
+#include <functional>
+#include <thread>
+#include <atomic>
+#include <utility>
+#include <pthread.h>
+#include <mutex>
+#include <condition_variable>
 #include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <time.h>
 #include <errno.h>
+
+
+class connector_t {
+public:
+    using callback_t = std::function<void(int fd)>;
+public:
+    connector_t(std::string  server_name, callback_t callback)
+    : s_name(std::move(server_name)), cb(std::move(callback)), should_stop(false) {
+        thread = std::thread([this]() {
+            worker_proc();
+        });
+    }
+
+    ~connector_t() {
+        should_stop = true;
+        cond.notify_one();
+
+        if (thread.joinable()) {
+            thread.join();
+        }
+    }
+
+    void start_connect() {
+        cond.notify_one();
+    }
+
+private:
+    void worker_proc() {
+        while (!should_stop) {
+            std::unique_lock lk(mutex);
+            cond.wait(lk);
+            if (should_stop) {
+                break;
+            }
+
+            int conn_fd = connect_to_server();
+            if (conn_fd == -1) {
+                fire_callback(-1);
+                continue;
+            }
+
+            int mem_fd = receive_fd(conn_fd);
+            close(conn_fd);
+            fire_callback(mem_fd);
+        }
+    }
+
+    int connect_to_server() {
+        struct sockaddr_un address{};
+        memset(&address, 0, sizeof(address));
+        address.sun_family = AF_UNIX;
+        snprintf(address.sun_path, UNIX_PATH_MAX, "%s", s_name.c_str());
+
+        int conn = socket(PF_UNIX, SOCK_STREAM, 0);
+        if (conn == -1) {
+            return -1;
+        }
+
+        if (connect(conn, (struct sockaddr *)&address, sizeof(struct sockaddr_un)) != 0) {
+            return -1;
+        }
+
+        return conn;
+    }
+
+    int receive_fd(int conn_fd) {
+        struct msghdr msgh{};
+        struct iovec iov{};
+        union {
+            struct cmsghdr cmsgh;
+            char   control[CMSG_SPACE(sizeof(int))];
+        } control_un{};
+        struct cmsghdr* cmsgh;
+
+        char placeholder = 0;
+        iov.iov_base = &placeholder;
+        iov.iov_len = sizeof(char);
+
+        msgh.msg_name = nullptr;
+        msgh.msg_namelen = 0;
+        msgh.msg_iov = &iov;
+        msgh.msg_iovlen = 1;
+        msgh.msg_control = control_un.control;
+        msgh.msg_controllen = sizeof(control_un.control);
+
+        if (recvmsg(conn_fd, &msgh, 0) != 1) {
+            return -1;
+        }
+
+        cmsgh = CMSG_FIRSTHDR(&msgh);
+        if (cmsgh == nullptr || cmsgh->cmsg_level != SOL_SOCKET || cmsgh->cmsg_type != SCM_RIGHTS) {
+            return -1;
+        }
+
+        return *((int *) CMSG_DATA(cmsgh));
+    }
+
+
+    void fire_callback(int fd) {
+        cb(fd);
+    }
+
+private:
+    std::string s_name;
+    callback_t cb;
+    std::thread thread;
+    volatile std::atomic<bool> should_stop;
+    std::condition_variable cond;
+    std::mutex mutex;
+};
+
 
 static void error(char *msg) {
     perror(msg);
